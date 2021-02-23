e94263d13fa544b754d0eb69f8ffbb6985a4e7b5
Himanshu Dagdi <dagdih@gmail.com>
2021-02-23 13:36:25 +0530

    few tweaks

--- learn-express/main.js
+++ learn-express/main.js
@@ -1,17 +1,22 @@
 const express = require('express')
 const app = express()
+const path = require("path")
 app.use(express.urlencoded({extended:false}))
+app.use(express.static(path.join(__dirname, "public")))
+
+app.set("view engine", "ejs")
+app.set("views", path.join(__dirname, "views"))
 
 // We can use template engine (ejs, pug, ...) for the html part
 app.get('/', (req, res) => {
-  res.send(`
-    <hmtl><body>
-      <form action='/result' method='post'>
-        rcb will <input name='q'> </input> this year !
-        <button> submit </button>
-      </form>
-    </body></html>
-  `)
+  res.render("home", {
+    apis: [
+      { name: "home-get", desc: "homepage get req" },
+      { name: "about-get", desc: "about get req" },
+      { name: "result-get", desc: "result get req" },
+      { name: "result-post", desc: "result post req" }
+    ]
+  })
 })
 app.get('/about', (req, res) => {
   res.send("Myself HimanD ;)")
